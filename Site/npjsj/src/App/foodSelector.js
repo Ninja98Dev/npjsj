586e61804d192238718f9b5442aee50d66f0b38a
Tom7893 <tomas.becak7@gmail.com>
2023-05-03 14:26:54 +0200

    Refactoring + tab view test

--- Site/npjsj/src/App/foodSelector.js
+++ Site/npjsj/src/App/foodSelector.js
@@ -1,10 +1,32 @@
-import { renderMenu } from '../index.js'
+import { renderMenu, renderFoodSelector } from '../index.js'
+
+let currentTab = 0;
 
 export function FoodSelector() {
 	return (
 		<div class='elements'>
-			<button id='backButton' onClick={renderMenu}>←</button>
-			<h1 align='center'>Výber jedla</h1>
+			<nav>
+				<button id='backButton' onClick={renderMenu}>←</button>
+			</nav>
+			<button id='tabButton' onClick={() => switchTab(0)}>Materská škola</button>
+			<button id='tabButton' onClick={() => switchTab(1)}>Stredná škola</button>
+			<fieldset>
+				{tab()}
+			</fieldset>
 		</div>
 	);
+}
+function switchTab(tab){
+	currentTab = tab;
+	renderFoodSelector();
+}
+function tab() {
+	if (currentTab == 0)
+	{
+		return ( <b>MŠ</b> );
+	}
+	else
+	{
+		return ( <b>SŠ</b> );
+	}
 }
