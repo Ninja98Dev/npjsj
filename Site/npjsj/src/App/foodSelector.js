8ea31bbd8f0f17efd1a77398fe12f4015fc60740
Tom7893 <tomas.becak7@gmail.com>
2023-06-05 16:32:28 +0200

    foods addition test 1

--- Site/npjsj/src/App/foodSelector.js
+++ Site/npjsj/src/App/foodSelector.js
@@ -38,23 +38,50 @@ class TAB {
 class TABLING {
 	constructor(name) {
 		this.name = name;
+		this.foodLines = [];
 	}
 	content() {
 		if (this.name == "") return (<div></div>);
 		else
 		return (
-		<fieldset>
-			<legend>{this.name}</legend>
-		</fieldset>
+		<div>
+			<fieldset>
+				<legend>{this.name}</legend>
+				{this.elementLines()}
+				<button style={{right: 5, width: 25, backgroundColor: "green", color: "lightgreen", fontSize: 18, height: 22}} onClick={() => addFood(this)}>+</button>
+			</fieldset>
+			
+		</div>
+		);
+	}
+	elementLines() {
+		const returnable = [];
+		for (let i = 0; i < this.foodLines.length; i++)
+		{
+			returnable.push(this.foodLines[i].content());
+			returnable.push(<hr style={{marginTop: 0}}></hr>);
+		}
+		return returnable;
+	}
+}
+class FoodTab {
+	constructor(name) {
+		this.name = name;
+	}
+	content() {
+		return (
+		<div>
+			{this.name}
+		</div>
 		);
 	}
 }
-
 let tabMS = new TAB();
 tabMS.tablings = [new TABLING("Raňajky"), new TABLING("Desiata"), new TABLING("Obed"), new TABLING(""), new TABLING("Olovrant"), new TABLING("Večera")];
 let tabSS = new TAB();
 tabSS.tablings = [new TABLING("Raňajky"), new TABLING("Desiata"), new TABLING("Obed 1"), new TABLING("Obed 2"), new TABLING("Olovrant"), new TABLING("Večera")];
 let currentTab = tabMS;
+
 
 export function FoodSelector() {
 	return (
@@ -79,5 +106,9 @@ function currentColor(num) {
 }
 function switchTab(tab){
 	currentTab = tab;
+	renderFoodSelector();
+}
+function addFood(tabling) {
+	tabling.foodLines.push(new FoodTab("nejake jedlo"));
 	renderFoodSelector();
 }
